e646a696d02f355d951141dacf6965d93bf85c7c
Rayna McGinnis <raynamcginnis@gmail.com>
2018-12-10 16:30:02 -0700

    updating javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,7 @@
 
-//ASSIGN VARIABLES
+$(document).ready(function () {
 
-// var user
-// var userName
-// var userHealth
-// var opponent
-// var opponentName
-// var opponentHaelth
-// var userAttack
-// var userAttackPower
-// var userCounterAttack
-// var opponentAttack
-// var opponentAttackPower
-// var opponentCounterAttack
-// var characterList = ["Offred","The Commander","Aunt Lydia","Serena"]
+//ASSIGN VARIABLES
 
 var offred;
 var theCommander;
@@ -24,75 +12,75 @@ var characterSelection = [];
 var defender = null;
 var defenders = [];
 
-$(document).ready(function () {
-
 
 //create array of characters and character traits included name, health, attack, counter attack, image
 // attack points should increase every time by adding itself to the total, counter attack power never changes
+
 function startGame() {
 
 var characters = {
- Offred: {
-    hp: 150,
-    baseattack: 15,
+ offred: {
+    healthPoints: 150,
+    baseAttack: 15,
     attackPower: 15,
-    counterAttack: 10,
+    counterAttackPower: 10,
     name: "Offred",
-    id: 1,
+    id: 0,
     img: "./assets/images/offred.jpg"
  }
 
   theCommander: {
-      hp: 140,
-      baseattack: 14,
+      healthPoints: 140,
+      baseAttack: 14,
       attackPower: 14,
-      counterAttack: 10,
+      counterAttackPower: 10,
       name: "The Commander",
-      id: 2,
+      id: 1,
       img: "./assets/images/theCommander.jpg"
   }
 
    auntLydia: {
-       hp: 130,
-       baseattack: 13,
+       healthPoints: 130,
+       baseAttack: 13,
        attackPower: 13,
-       counterAttack: 20,
+       counterAttackPower: 20,
        name: "Aunt Lydia",
-       id: 3,
+       id: 2,
        img: "./assets/images/auntLydia.jpg"
    } 
   
    Serena: {
-       hp: 90,
-       baseattack: 16
+       healthPoints: 90,
+       baseAttack: 16
        attackPower: 16,
-       counterAttack: 18
+       counterAttackPower: 18
        name: "Serena",
-       id: 4,
+       id: 3,
        img: "./assets/images/offred.jpg"
    }
-
-}
 
 //reset to begin game
 
 character = null;
 defenders = [];
 defender = null;
-characterSelection = ["Offred","The Commander","Aunt Lydia","Serena"];
+characterSelection = [offred, theCommander, auntLydia, serena];
 
 $("#character", "#defenderArea", "#defender", "#status").empty();
 
 //Selecting the characters
 
-$.each(".characterSelection", function (index, character){
-var newCharacterDiv = $("<div>").addClass("character panel panel-success").attr("id", character.id)''
+$.each(".characterSelection", function (index, character) {
+      
+// create a div for each character to display character selection at start of the game
+
+var newCharacterDiv = $("<div>").addClass("character panel panel-success").attr("id", character.id);
 
             $("<div>").addClass("panel-heading").html(character.name).appendTo(newCharacterDiv);
 			$("<div>").addClass("panel-body").append("<img src='" + character.img + "'>").appendTo(newCharacterDiv);
 			$("<div>").addClass("panel-footer").append("<span class='hp'>" + character.healthPoints + "</span>").appendTo(newCharacterDiv);
 
-			// put it on the HTML
+			// new div to character selection
 			$("#characterSelection").append(newCharacterDiv);
 
 });
@@ -108,8 +96,8 @@ $(".character").on("click", function() {
 
         // loop through character array
         $.each(characterSelection, function(index, character) {
-            // add unselected characters to enemies array
-            if(character.id !== charId) {
+//Move other opponents to enemies array
+                if(character.id !== charId) {
                 defenders.push(character);
                 $("#"+character.id).removeClass("character panel-success").addClass("defender panel-danger").appendTo("#defenderArea");
             } else {
@@ -118,22 +106,40 @@ $(".character").on("click", function() {
         });
 
 
-//Move remaining characters to opponent div
+
+// add click event after defender class has been added
+$(".defender").on("click", function() {
+    if(defender === null) {
+        var defenderId = parseInt($(this).attr("id"));
+        console.log(this);
+        defender = characterSelection[defenderId];
+        $("#" + defenderId).appendTo("#defender");
+    }
+});
+}
+});
+
+//hide restart button
+
+$("#restart").hide();
+}
+
+startGame();
+
 
 //use on click to select opponent to attack
 
 $("#attack").on("click", function() {
 		
     if(character !== null && character.healthPoints > 0 && defenders.length > 0) {
-        // game status in text
         var status = "";
 
         // when defender has been selected
-        debugger;
+    
         if(defender !== null) {
-            // decrease defender HP by character attack power
+            // decrease defender HP
             defender.healthPoints -= character.attackPower;
-            status += "You tweeted " + defender.name + " for " + character.attackPower + " shame.<br>";
+            status += "You attacked " + defender.name + " for " + character.attackPower + " damage.<br>";
 
             console.log("Defender: ",defender.name,defender.healthPoints);
 
@@ -142,7 +148,7 @@ $("#attack").on("click", function() {
 
             // decrease character HP by defender counter attack power
             character.healthPoints -= defender.counterAttackPower;
-            status += defender.name + " @'d you back for " + defender.counterAttackPower + " shame.<br>";
+            status += defender.name + " attacked you back for " + defender.counterAttackPower + " damage.<br>";
 
             console.log("Character: ",character.name,character.healthPoints);
 
@@ -152,13 +158,15 @@ $("#attack").on("click", function() {
             // increase character attack power by base attack power
             character.attackPower += character.baseAttack;
 
+            //win or lose
+
             // when character is defeated
             if(character.healthPoints <= 0) {
                 status = "YOU HAVE BEEN DEFEATED. GAME OVER!!!!";
                 $("#restart").show();
             } else if(defender.healthPoints <= 0) {	
                 // when defender is defeated
-                status = "You have defeated " + defender.name + ", you can start another beef with a new foe.";
+                status = "You have defeated " + defender.name + "!";
 
                 // clear defender selection
                 $("#defender").empty();
@@ -170,25 +178,22 @@ $("#attack").on("click", function() {
 
             // when no defenders left
             if(defenders.length === 0) {
-                status = "You won the beef!";
+                status = "CONGRATULATIONS, YOU WON!";
                 $("#restart").show();
             }
         } else {
-            status = "No foes here.";
+            status = "No enemies left!";
         }
 
         $("#status").html(status);
     }
 })
 
+$("#restart").on("click", function () {
+    startGame();
+})
 
-//functionality of attack button
-
-//select next opponent from selection area
-
-//win or lose
-
-}
+});
 
 //create read me files
 
